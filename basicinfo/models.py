e53aa47ed15a1368c8725df975bf772f482acdca
pydo <964683112@qq.com>
2016-01-19 17:09:34 +0800

    同步了数据库
    同步了数据库，并且设计了第一个模型，没有把问题分开，下一步准备解决中文乱码问题，ps，django1.9好帅哦

--- basicinfo/models.py
+++ basicinfo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class BasicInfo(models.Model):
+    address_text = models.CharField(max_length = 200)
+    code_text = models.CharField(max_length = 10)
+    schoolname_text = models.CharField(max_length = 200)
+    location_text = models.CharField(max_length = 200)
+    contact_text= models.CharField(max_length = 200)
+    tel_text= models.CharField(max_length = 200)
+    email_text= models.CharField(max_length = 200)
+    grade_text= models.CharField(max_length = 200)
+    teachersnum_text= models.CharField(max_length = 200)
+    studentnum_text= models.CharField(max_length = 200)
+    fill_date = models.CharField(max_length = 200)
+    
