6a36e7e652630b8ceb549b09d99fc03f62939008
pydo <964683112@qq.com>
2016-01-22 17:00:50 +0800

    修改试题

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class QuestionSort(models.Model):
+	questsort_text = models.CharField(max_length = 200)
 
+	def __str__(self):
+		return self.questsort_text
 
 class Question(models.Model):
 	question_text = models.CharField(max_length = 200)
