af8330cd1b6b06ec1a1abed4deaff28fc6f7fddf
pydo <964683112@qq.com>
2016-01-27 10:47:44 +0800

    添加登录路径url

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
 	url(r'^$',views.index,name = 'index'),
 	url(r'^(?P<poll_id>[0-9]+)/$', views.detail, name='detail'),
-	 url(r'^(?P<poll_id>[0-9]+)/results/$', views.results, name='results'),
-
-]
+	url(r'^(?P<poll_id>[0-9]+)/results/$', views.results, name='results'),
+	url(r'^login/$', views.login, name='login'),
+	]
 
