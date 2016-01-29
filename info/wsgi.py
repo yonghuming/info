67003e4b53f530126d30199a64ab1d52ef385af6
pydo <964683112@qq.com>
2016-01-29 17:32:28 +0800

    wsgi.py配置
    默认生成的不适合Apache配置的环境

--- info/wsgi.py
+++ info/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for info project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
-"""
-
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "info.settings")
+from os.path import join,dirname,abspath
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+import sys
+sys.path.insert(0,PROJECT_DIR)
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "info.settings")
-
 application = get_wsgi_application()
